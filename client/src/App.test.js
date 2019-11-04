8bb0b9a3e3971d61e2a043cf0bb845e6561539d0
Matt Mayes <mattamayes@gmail.com>
2019-11-04 14:37:46 -0600

    initial

---
+++ client/src/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// server prob not running at this point in time
+// *could* mock up something that would return some json
+it('renders without crashing', () => {
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+  // ReactDOM.unmountComponentAtNode(div);
+});
