9ac3c5e8e351da945a72d2b53eba6793893f1c40
Matt Mayes <mattamayes@gmail.com>
2019-12-01 15:34:59 -0600

    Fixed dockerhub id

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-docker build -t stoptime/multi-client:latest -t stoptime/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stoptime/multi-server:latest -t stoptime/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stoptime/multi-worker:latest -t stoptime/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push stoptime/multi-client:lastest
-docker push stoptime/multi-server:lastest
-docker push stoptime/multi-worker:lastest
-docker push stoptime/multi-client:$SHA
-docker push stoptime/multi-server:$SHA
-docker push stoptime/multi-worker:$SHA
+docker build -t stoptime9/multi-client:latest -t stoptime9/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t stoptime9/multi-server:latest -t stoptime9/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t stoptime9/multi-worker:latest -t stoptime9/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push stoptime9/multi-client:lastest
+docker push stoptime9/multi-server:lastest
+docker push stoptime9/multi-worker:lastest
+docker push stoptime9/multi-client:$SHA
+docker push stoptime9/multi-server:$SHA
+docker push stoptime9/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stoptime/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stoptime/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stoptime/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=stoptime9/multi-server:$SHA
+kubectl set image deployments/client-deployment client=stoptime9/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=stoptime9/multi-worker:$SHA
